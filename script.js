e28bd4ab1d1db2178555c7b0ea33abb1a66e6e59
jeasmynballete <165183681+jeasmynballete@users.noreply.github.com>
2024-03-29 21:13:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function greetUser(){
+  console.log('Welcome to the Car Show WEBSITE!!');
+}
+greetUser(); 
+function displayCarModels(car);
+console.log('The car models are:');
+console.log(car);
+}
+displayCarModels ('Toyota');
+displayCarModels ('Honda');
+displayCarModels ('BMW');
+displayCarModels ('Mercedes');
+displayCarModels ('Audi');
+displayCarModels ('Ford');
+displayCarModels ('Chevrolet');
+displayCarModels ('Kia');
+displayCarModels i love u sam
+function displayMessage(yay){
+  console.log(message);
+}
+
+greetUser();
+
+displayMessage('Please explore our website and enjoy the cars!')
+
